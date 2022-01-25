d1d28d924b8c92f51d5dbe507a4b0abb0ef18ba9
jeongho2 <jeongho.kim@snuailab.ai>
2022-01-25 10:49:43 +0900

    mot2coco.py 수정

--- convertDataset/mot/mot2coco.py
+++ convertDataset/mot/mot2coco.py
@@ -11,14 +11,12 @@ import math
 
    COCO : bbox" : [x,y,width,height], 
 """
-def convert2coco(dir_list):
+def convert2coco(dir_list, val=False):
     categories = [{'id': 0, 'name': 'person', "supercategory": "none"}]
     images = []
     annotations = []
-
-    print(dir_list)
     img_id_offset = 0
-
+    ann_id = 0 
     for dir_path in dir_list:
         print("dir_path : " + dir_path)
         s = dir_path.split('.')
@@ -26,10 +24,8 @@ def convert2coco(dir_list):
         f = open(os.path.join(dir_path, 'gt/gt.txt'), 'r')
         gt = []
         for line in f:
-            info = [int(float(i)) for i in line[:-1].split(',')]
-            # info = [print(int(float(i))) for i in line[:-1].split(',')]
-            # info = [print(round(float(i))) for i in line[:-1].split(',')]
-            # info = [int(round(float(i))) for i in line[:-1].split(',')]
+            # info = [int(float(i)) for i in line[:-1].split(',')]
+            info = [int(round(float(i))) for i in line[:-1].split(',')]
             gt.append(info)
         f.close
         config = configparser.ConfigParser()
@@ -38,76 +34,69 @@ def convert2coco(dir_list):
         w = int(config['Sequence']['imWidth'])
 
         cls2cat = [1, 7]
-        ann_id = 0
-        img_list = np.unique(np.array(gt)[:,0])
+        img_list = np.unique(np.array(gt)[:,0]).tolist()
+        img_id = 0
+
+        if val:
+            img_list = np.random.choice(img_list, int(len(img_list)/10)).tolist()
         for im in img_list:
-            # print(im)
-            id = im + img_id_offset -1
+            id = img_id + img_id_offset
             file_name = f'{im:06d}.jpg'
             # file_name = dir_path + file_name
             # file_name = os.path.join(dir_path, 'img1', file_name)
-            # print("dir_path : " + dir_path + "file_name :" + file_name)
             file_name = dir_path + '_' + file_name
             images.append(
                 {
-                'id': int(id),
-                'file_name': file_name,
-                'height': h,
-                'width': w}
+                    'id': int(id),
+                    'file_name': file_name,
+                    'height': h,
+                    'width': w
+                }
             )
-
+            img_id += 1
 
         for ann in gt:
-            if ann[7] not in cls2cat and ann[-1] > 0.5: continue
-            # print(ann[0])
-            image_id = ann[0] + img_id_offset -1
-            category_id = 0
-            bbox = ann[2:6]
+            print('annn')
             # x, y, width, height
-            if bbox[0] < 0:
-                bbox[0] = 0
+            bbox = ann[2:6]
 
-            if bbox[0] > w:
-                bbox[0] = w
+            # boundary check
+            if bbox[0] < 0: bbox[0] = 0
+            if bbox[0] > w: bbox[0] = w
             
-            if bbox[1] < 0:
-                bbox[1] = 0
+            if bbox[1] < 0: bbox[1] = 0
+            if bbox[1] > h: bbox[1] = h
+
+            if bbox[2] < 0: bbox[2] = 0
+            if (bbox[0] + bbox[2]) > w: bbox[2] = w - bbox[0]
             
-            if bbox[1] > h:
-                bbox[1] = h
+            if bbox[3] < 0: bbox[3] = 0
+            if (bbox[1] + bbox[3]) > h: bbox[3] = h - bbox[1]
 
-            if bbox[2] < 0:
-                bbox[2] = 0
-            
-            if (bbox[0] + bbox[2]) > w:
-                # print("x + width > w")
-                # print("x : " + str(bbox[0]) + " width : " + str(bbox[2]) + "w :" + str(w))
-                bbox[2] = w - bbox[0]
-                # print("after width : " + str(bbox[2]))
-            
-            if bbox[3] < 0:
-                bbox[3] = 0
+            if ann[7] not in cls2cat or ann[-1] < 0.5: 
+                print('not cls2cat')
+                continue #
+            if bbox[3]/bbox[2] < 0.5 or bbox[3]/bbox[2] > 6: 
+                print('not 0.5 6')
+                continue # 6 가로 세로 비율
 
-            if (bbox[1] + bbox[3]) > h:
-                # print("y + height > h")
-                # print("y : " + str(bbox[1]) + " height : " + str(bbox[3]) + "h :" + str(h))
-                bbox[3] = h - bbox[1]
-                # print("after height : " + str(bbox[3]))
-
-
-            # print('height :' + str(h) + ' width : ' + str(w))
-            # print(bbox)
-            annotations.append(
-            {
-                'id': ann_id,
-                'image_id': image_id,
-                'category_id': category_id,
-                'bbox': bbox}
-            )
-            ann_id += 1
-        
-        img_id_offset += len(img_list)
-        # print(img_id_offset)
+            if ann[0] in img_list:
+                image_id = img_list.index(ann[0]) + img_id_offset
+                category_id = 0                
+                w = images[image_id]['width']
+                h = images[image_id]['height']
+                annotations.append(
+                    {
+                        'id': ann_id,
+                        'image_id': image_id,
+                        'category_id': category_id,
+                        'bbox': bbox,
+                        'area': bbox[2] * bbox[3],
+                        'iscrowd': 0
+                    }
+                )
+                ann_id += 1
+        img_id_offset += img_id
 
     ann_dict = {
         'categories': categories,
@@ -117,13 +106,13 @@ def convert2coco(dir_list):
     return ann_dict
 
 
-train_dir = ['MOT17-02', 'MOT17-04', 'MOT17-13', 'MOT20-02', 'MOT17-10', 'MOT17-09', 'MOT20-05']
+train_dir = ['MOT17-02', 'MOT17-04', 'MOT17-09', 'MOT17-10', 'MOT17-13', 'MOT20-02', 'MOT20-05']
 val_dir = ['MOT20-01', 'MOT20-03']
 train_json = convert2coco(train_dir)
 with open('train.json', 'w') as fp:
     json.dump(train_json, fp)
 
-val_json = convert2coco(val_dir)
+val_json = convert2coco(val_dir, val=False)
 with open('val.json', 'w') as fp:
     json.dump(val_json, fp)
 
